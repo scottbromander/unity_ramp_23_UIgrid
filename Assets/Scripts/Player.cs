4c22af6eed9d691d8f07f08acf422d25ca97cf47
Scott Bromander <scott@primeacademy.io>
2016-06-27 16:01:24 -0500

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private PlayerInventoryDisplay playerInventoryDisplay;
+	private bool carryingStar = false;
+
+	// Use this for initialization
+	void Start () {
+		playerInventoryDisplay = GetComponent<PlayerInventoryDisplay> ();
+		playerInventoryDisplay.OnChangeCarryingStar (carryingStar);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.CompareTag("Star")){
+			carryingStar = true;
+			playerInventoryDisplay.OnChangeCarryingStar (carryingStar);
+			Destroy (hit.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour {
+	public Text starText;
+
+	public void OnChangeCarryingStar(bool carryingStar){
+		string starMessage = "No star :(";
+		if(carryingStar) starMessage = "Carrying Star :)";
+		starText.text = starMessage;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
